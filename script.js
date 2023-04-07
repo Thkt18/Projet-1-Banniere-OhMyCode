77be40e7c6b8c88cd8ba07b434ad39b09dd644b8
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2023-04-07 15:38:48 +0200

    Disparation de la bannière  après acceptation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('connecté'); 
+
+const btnSuccess = document.querySelector('.btn-success');
+console.log('btnSuccess');
+
+const cookies = document.querySelector('.cookies');
+console.log('cookies');
+
+btnSuccess.addEventListener ('click', function() {
+    cookies.style.opacity ="0";
+});
